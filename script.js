bc7fb6b42b126250f21053f4aad4b9901e2e6401
Ling Ying Yang <lingyingy9447@hstat.org>
2023-04-07 15:17:08 -0400

    set-up

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 kaboom()
+
+//SPRITES
+loadSprite('toto', 'img/main.png')
+loadSprite('coin', 'img/coin.png')
+// loadSprite('enemy', 'img/enemy.png')
+loadSprite('grass', 'img/grass.png')
+loadSprite('stone', 'img/stone.png')
+loadSprite('door', 'img/door.png')
+
+// LEVELS
+
+const LEVELS =
+    [
+        '                          ',
+        '                          ',
+        '                          ',
+        '                          ',
+        '                          ',
+        '     $$$                  ',
+        '                          ',
+    ]
+
+
+// DEFINGING SPRITES
+
+const levelConf = {
+    width: 64,
+    height: 64, 
+
+    '=': () => [
+        sprite('grass'),
+        area(),
+        solid(),
+        origin('bot'),
+    ],
+    '$': () => [
+        sprite('coin'),
+        area(),
+        solid(),
+        origin('bot'),
+    ],
+
+}
