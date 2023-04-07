76293f60feb696deaf800d33b9da058d7c05b09d
Ling Ying Yang <lingyingy9447@hstat.org>
2023-04-07 15:29:24 -0400

    something

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ kaboom()
 //SPRITES
 loadSprite('toto', 'img/toto.png')
 loadSprite('coin', 'img/coin.png')
-// loadSprite('enemy', 'img/enemy.png')
+loadSprite('enemy', 'img/enemy.png')
 loadSprite('grass', 'img/grass.png')
 loadSprite('stone', 'img/stone.png')
 loadSprite('door', 'img/door.png')
